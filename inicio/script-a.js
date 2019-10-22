5994c0a55d09fa1e2c077d328d172958c235e767
jonathan113 <50118036+jonathan113@users.noreply.github.com>
2019-10-22 20:06:08 -0300

    Update script-a.js

--- inicio/script-a.js
+++ inicio/script-a.js
@@ -19,18 +19,23 @@ div4.style.visibility = "hidden";
 
 button.onclick = function click1(){
   p.style.visibility = "visible";
+  button.visibility = "hidden";
 }
 
   button1.onclick = function b1(){
     div1.style.visibility = "visible";
+    button1.visibility = "hidden";
   }
 
 button2.onclick = function ba(){
 div2.style.visibility = 'visible';
+  button2.visibility = "hidden";
 }
 button3.onclick = function click1(){
   div4.style.visibility = "visible";
+  button3.visibility = "hidden";
 }
 love.onclick = function click1(){
   div3.style.visibility = "visible";
+  love.visibility = "hidden";
 }
