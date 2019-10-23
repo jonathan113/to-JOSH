f07832a5e11b0ed6490dcb98efe3365ede08b219
jonathan113 <50118036+jonathan113@users.noreply.github.com>
2019-10-22 21:11:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let txt = document.getElementById("txt");
 let button = document.getElementById("button");
 let href = document.getElementById('href');
 let label = document.getElementById('label');
-
+let rta;
 
 href.style.visibility = "hidden";
 
@@ -10,6 +10,7 @@ button.onclick = function click(){
   if(txt.value == "iloveyou"){
     console.log('iloveyou');
       href.style.visibility = "visible";
+    rta = true;
   }else{
     console.log('hola');
     label.innerHTML = "try again";
